44224164c4c83c9197697409f24032a133654aa8
tcassaert <tcassaert@inuits.eu>
2020-02-11 17:23:21 +0100

    Updated copyright

--- lib/board.go
+++ lib/board.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 Thomas Cassaert <tcassaert@inuits.eu>
+Copyright © 2020 Thomas Cassaert <tcassaert@inuits.eu>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
